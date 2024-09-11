517e1eb308640e20eb20ff6f6c264ec0681a9264
exphoenee <vbozzay@aegex.com>
2024-09-11 14:22:30 +0200

    add depoly command

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import {svelte} from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/arrganizerdocs/",
   plugins: [svelte()],
   resolve: {
     alias: {
